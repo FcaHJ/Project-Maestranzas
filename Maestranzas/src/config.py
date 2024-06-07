a5f9843a6b33b6736e0623b1125369c866661977
FcaHJ <fran.herrera@duocuc.cl>
2024-06-07 17:11:00 -0400

    Modificacion nombre BD

--- Maestranzas/src/config.py
+++ Maestranzas/src/config.py
@@ -6,8 +6,8 @@ class DevelopmentConfig(Config):
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '123456'
-    MYSQL_DB = 'flask_users'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'maestranzas'
 
 config = {
     'development': DevelopmentConfig
